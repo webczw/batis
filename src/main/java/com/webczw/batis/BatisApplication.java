5c0721244b82af29396c7a6e53c185aced185019
webczw <webczw@163.com>
2019-01-30 23:25:41 +0800

    新增数据库SQL解析项目

---
+++ src/main/java/com/webczw/batis/BatisApplication.java
@@ -0,0 +1,14 @@
+package com.webczw.batis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatisApplication.class, args);
+    }
+
+}
+
